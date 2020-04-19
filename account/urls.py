259964c6de4ecec427f67110c2b455c232631064
RadGrabowski <radek18g@gmail.com>
2020-04-19 19:41:02 +0200

    added registration form to create new account

--- account/urls.py
+++ account/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('password-reset/confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('register/', views.register, name='register'),
 ]
