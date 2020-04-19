c4e7c4020d4175b5fbc6ca96b59a113ffb5e6dca
RadGrabowski <radek18g@gmail.com>
2020-04-19 23:11:43 +0200

    extended user with date of birth and picture, added option to edit it

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bookmarks/static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 # LOGOUT_REDIRECT_URL = reverse_lazy('login')
 LOGIN_URL = reverse_lazy('login')
