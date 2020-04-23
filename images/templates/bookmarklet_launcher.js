403e23ea5eaf8e71036c58d336bcc9e45f4463ca
RadGrabowski <radek18g@gmail.com>
2020-04-23 21:32:04 +0200

    new AJAX request to handle image likes

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -4,9 +4,7 @@
     } else {
         if (window.bookmarklet !== undefined) {
         } else {
-            document.body.appendChild(document.createElement('script')).
-                src = 'http://127.0.0.1:8000/static/js/bookmarklet.js?r=' + Math.
-                floor(Math.random() * 99999999999999999999);
+            document.body.appendChild(document.createElement('script')).src = 'http://127.0.0.1:8000/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 99999999999999999999);
         }
     }
 })();
--- images/urls.py
+++ images/urls.py
@@ -5,5 +5,6 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create'),
-    path('detail/<id_>/<slug>', views.image_detail, name='detail')
+    path('detail/<id_>/<slug>', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like')
 ]
