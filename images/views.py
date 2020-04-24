85bc3a6528397774e664793b8efaf91c9212be9d
RadGrabowski <radek18g@gmail.com>
2020-04-24 21:21:06 +0200

    added AJAX infinite scroll function

--- images/views.py
+++ images/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.contrib import messages
-from django.http import JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse, HttpResponse
 from .forms import ImageCreateForm
 from .models import Image
+from common.decorators import ajax_required
 
 
 @login_required
@@ -29,6 +31,7 @@ def image_detail(request, id_, slug):
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -45,3 +48,21 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'ko'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
